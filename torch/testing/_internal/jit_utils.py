1361f62af40db9dd0edef9716de9ddcf74b1a5a0
eellison <eellison@fb.com>
2020-02-19 10:48:09 -0800

    add back on jitter tests

--- torch/testing/_internal/jit_utils.py
+++ torch/testing/_internal/jit_utils.py
@@ -170,9 +170,9 @@ class JitTestCase(TestCase):
 
     def emitFunctionHook(self, func):
         # func has invalid names for export, skip the jitter check
-        inline_everything = torch._C._jit_get_inline_everything_mode()
-        if func.name == "<lambda>" or "aten::" in func.name or not inline_everything:
+        if func.name == "<lambda>" or "aten::" in func.name :
             return
+        self.run_pass("inline", func.graph)
         self._compared_saved_loaded(func)
 
     def emitModuleHook(self, module):
